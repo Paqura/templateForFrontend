748e615ce2fb40f2103f8ff12dceedcb44b010b5
slavals <slavals616@gmail.com>
2017-11-11 16:41:56 +0300

    _

--- src/js/components/ButtonToggle/_buttonToggle.js
+++ src/js/components/ButtonToggle/_buttonToggle.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded',() => {
 
   const button  = document.querySelector('.button');
+  const input = document.querySelector('.input');
+  const text = document.querySelector('p');
+  let name = 'Некто';
 
   button.addEventListener('click', event => {
     event.preventDefault();
 
-    let text = document.querySelector('p');
-    text.classList.remove('v-hidden');
+    text.classList.toggle('v-hidden');
+    changeName();
   });
+
+  input.addEventListener('input', event => {
+      name = event.target.value;
+      changeName();
+  });
+
+  function changeName() {
+    text.innerText = 'Привет' + ' ' + name;
+  }
+
 });
 
